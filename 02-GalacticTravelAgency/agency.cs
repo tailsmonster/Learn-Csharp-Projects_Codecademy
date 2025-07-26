5caf26de0fa78e02b8eff2728ac33f9493264890
Nico <aroca.twelve@proton.me>
2025-07-26 10:28:08 -0400

    added project 2, var type is like auto btw

---
+++ 02-GalacticTravelAgency/agency.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class GalacticTravelAgency { 
+  public static void Main(string[] args) {
+    // Your code goes here
+    string passengerName = "Zara";
+    int passengerAge = 28;
+    string ticketType = "First Class";
+    string preferredPlanet = "Mars";
+
+    passengerAge++;
+    double passengerAgeDouble = (double) passengerAge; // explicit
+    double passengerAgeDouble2 = passengerAge; // implicit
+    string passengerAgeString = Convert.ToString(passengerAge); // method
+
+    Console.WriteLine(passengerName);
+    Console.WriteLine(passengerAge);
+    Console.WriteLine(passengerAgeDouble);
+    Console.WriteLine(passengerAgeDouble2);
+    Console.WriteLine(passengerAgeString);
+    Console.WriteLine(ticketType);
+    Console.WriteLine(preferredPlanet);
+  }
+}
